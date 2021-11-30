15d98e004c82a73e08a3b2b465e58387085c926c
“haibao” <837098975@qq.com>
2021-11-30 22:52:06 +0800

    30

--- ants/main.go
+++ ants/main.go
@@ -1,1 +1,59 @@
-package ants
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/panjf2000/ants/v2"
+)
+
+var sum int32
+
+func myFunc(i interface{}) {
+	n := i.(int32)
+	atomic.AddInt32(&sum, n)
+	fmt.Printf("run with %d\n", n)
+}
+
+func demoFunc() {
+	time.Sleep(10 * time.Millisecond)
+	fmt.Println("Hello World!")
+}
+
+func main() {
+	//  例子1 单个提交  不支持参数
+	p1, _ := ants.NewPool(20, ants.WithExpiryDuration(4))
+	defer p1.Release()
+	runTimes := 1000
+	var wg sync.WaitGroup
+	syncCalculateSum := func() {
+		demoFunc()
+		wg.Done()
+	}
+	for i := 0; i < runTimes; i++ {
+		wg.Add(1)
+		_ = p1.Submit(syncCalculateSum)
+	}
+	wg.Wait()
+	fmt.Printf("running goroutines: %d\n", ants.Running())
+	fmt.Printf("finish all tasks.\n")
+
+
+	// 列子2 例子1 单个提交  支持参数
+	p, _ := ants.NewPoolWithFunc(10, func(i interface{}) {
+		myFunc(i)
+		wg.Done()
+	})
+	defer p.Release()
+	// Submit tasks one by one.
+	for i := 0; i < runTimes; i++ {
+		wg.Add(1)
+		// 这个就是发送，相当于上述普通的pool的submit，唯一不同的是参数，因为这个发送的同类型的方法，加入逻辑代码时注意
+		_ = p.Invoke(int32(i))
+	}
+	wg.Wait()
+	fmt.Printf("running goroutines: %d\n", p.Running())
+	fmt.Printf("finish all tasks, result is %d\n", sum)
+}
--- websocket/go.mod
+++
@@ -1,5 +0,0 @@
-module hb_websock
-
-go 1.16
-
-require github.com/gorilla/websocket v1.4.2
