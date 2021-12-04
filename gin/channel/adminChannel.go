1728e48026deed06745a1e40a588ccc6f05adb6d
“haibao” <837098975@qq.com>
2021-12-04 23:06:00 +0800

    4

--- gin/channel/adminChannel.go
+++ gin/channel/adminChannel.go
@@ -1,15 +1,19 @@
 package channel
 
-import "hb_gin/common"
+import (
+	"hb_gin/common"
+	"sync/atomic"
+)
 
 var AdminMessage chan string
 
-func NewAdminMessageChannel(){
-	AdminMessage = make(chan string,10)
-	for{
+func NewAdminMessageChannel() {
+	AdminMessage = make(chan string, 10)
+	for {
 		select {
-			case adminMessage := <-AdminMessage:
-				common.Log.Info(adminMessage)
+		case adminMessage := <-AdminMessage:
+			atomic.AddInt32(&chanLength, 1)
+			common.Log.Info(adminMessage)
 		}
 	}
 }
